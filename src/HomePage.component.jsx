adb5edcc312e881427320a3fa210af4dea7992f8
Mister Stino <misterstino@MacBook-Air-van-Mister.local>
2020-11-19 16:05:35 +0100

    created project with create react app and started on homepage

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='nav-container'>
+        <div className='catergory-card'>
+            <div className='text-box'>
+                <h1>Hats</h1>
+                <span>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='catergory-card'>
+            <div className='text-box'>
+                <h1>Jackets</h1>
+                <span>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='catergory-card'>
+            <div className='text-box'>
+                <h1>Sneakers</h1>
+                <span>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='catergory-card'>
+            <div className='text-box'>
+                <h1>Womans</h1>
+                <span>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='catergory-card'>
+            <div className='text-box'>
+                <h1>Mens</h1>
+                <span>SHOP NOW</span>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
